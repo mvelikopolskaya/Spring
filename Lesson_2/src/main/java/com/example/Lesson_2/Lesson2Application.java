774f989e9166cfd3a54d37d39c6a3bb302790329
Cindrom1 <cindrof@gmail.com>
2025-01-18 17:31:18 +0300

    added Lesson 2

--- Lesson_2/src/main/java/com/example/Lesson_2/Lesson2Application.java
+++ Lesson_2/src/main/java/com/example/Lesson_2/Lesson2Application.java
@@ -3,6 +3,9 @@ package com.example.Lesson_2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Используя Spring Boot, создайте простое веб-приложение,
+//которое приветствует пользователя на главной странице.
+
 @SpringBootApplication
 public class Lesson2Application {
 
