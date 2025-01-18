515f8faf8b39a4ce1c8a1d7e2f2a2e038b7f4f13
Cindrom1 <cindrof@gmail.com>
2025-01-18 17:27:51 +0300

    added Lesson 1

--- Lesson_1/myappmaven/src/main/java/lesson1/Person.java
+++ Lesson_1/myappmaven/src/main/java/lesson1/Person.java
@@ -19,30 +19,6 @@ public class Person {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     @Override
     public String toString() {
         return new ToStringBuilder(this)
